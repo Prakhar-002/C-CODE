3063f600ee31406ac1b7fa1a3d33743df7740a90
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2023-12-20 20:50:11 +0530

    Tree menuDriven

--- treeMenuDriven.c
+++ treeMenuDriven.c
@@ -8,16 +8,15 @@ struct Node{
 
 int idx = -1;
 
-struct Node *tree(int arr[] , int n){
-      if (idx == n-1){
+struct Node *tree(int arr[] ){
+      idx++;
+      if (arr[idx] == -1){ 
             return NULL;
       }
-      idx++;
-
       struct Node *newNode = malloc(sizeof(struct Node));
       newNode->data = arr[idx];
-      newNode->left = tree(arr , n);
-      newNode->right = tree(arr, n);
+      newNode->left = tree(arr);
+      newNode->right = tree(arr);
       return newNode;
 } 
 
@@ -51,24 +50,144 @@ void postOrder(struct Node *root ){
       printf("%d\t",root->data);
 }
 
+// #define max (a , b)  (((a) > (b)) ? (a) : (b) );
+
+int maxNo(int a , int b){
+      if (a> b){
+            return a;
+      }else{
+            return b;
+      }
+}
+
+
+int nodeSum(struct Node * root){
+      if (root == NULL){
+            return 0;
+      }
+      
+      int leftSum = nodeSum(root->left);
+      int rightSum = nodeSum(root->right);
+
+      return leftSum+rightSum + root->data;
+}
+
+int numberOfNodes(struct Node *root){
+      if(root == NULL){
+            return 0;
+      }
+
+      int lc = numberOfNodes(root->left);
+      int rc = numberOfNodes(root->right);
+
+      return lc + rc +1;
+}
+
+int height(struct Node *root){
+      if (root == NULL){
+            return 0;
+      }
+
+      int lh = height(root->left);
+      int rh = height(root->right);
+
+      return maxNo(lh , rh) +1;
+}
+
+struct Info{
+      int diameter;
+      int height;
+};
+
+struct Info* diameterOfTree(struct Node *root){
+      if (root == NULL){
+            struct Info *newNode = malloc(sizeof(struct Info));
+            newNode->diameter = 0;
+            newNode->height = 0;
+            return newNode;
+      }
+
+      struct Info* leftInfo = diameterOfTree(root->left);
+      struct Info* rightInfo = diameterOfTree(root->right);
+
+      int selfDia = maxNo(maxNo(leftInfo->diameter , rightInfo->diameter) , leftInfo->height + rightInfo->height + 1);
+      int selfHei = maxNo(leftInfo->height , rightInfo->diameter) +1;
+
+      struct Info * returnNode = malloc(sizeof(struct Info));
+      returnNode->diameter = selfDia ;
+      returnNode->height = selfHei ;
+
+      return returnNode;
+}
+
+struct Node *root;
+
+void arrToTree(){
+      printf("Enter the length of Tree array...\n");
+            int n;
+            scanf("%d",&n);
+            int arr[n];
+            printf("Enter the data of every node ( If child is not exist enter -1)\n");
+            for (int i = 0; i < n; i++){
+                  scanf("%d" , &arr[i]);
+            }
+            root= tree(arr);
+            preOrder(root);
+}
 
 void main(){
-      int arr [] ={4,5,8,3,7,9,10};
-      // struct Node *root= tree(arr,7);
-      struct Node * root = malloc(sizeof(struct Node));
-      root->data = 10;
-      root->left = malloc(sizeof(struct Node));
-      root->left->data = 12;
-      root->left->left = NULL;
-      root->left->right = NULL;
-      root->right = malloc(sizeof(struct Node));
-      root->right->data = 15;
-      root->right->left = NULL;
-      root->right->right = NULL;
-      printf("inOrder...\n");
-      inOrder(root);
-      printf("\npreOrder...\n");
-      preOrder(root);
-      printf("\npostOrder...\n");
-      postOrder(root);
+      printf("WELCOME to the world of Tree...\n");
+      while(1){
+            printf("\nEnter your choice...\n");
+            printf("1 for default Tree...\t2 for Input elements of array to convert it into Tree...\t3 for inOrder traversing...\t4 for preorder traversing...\t5 for postOrder traversing...\t6 for Number of nodes in Tree...\t 7 for Height of tree...\t8 for sum of data of nodes of tree...\t9 for Diameter of tree...\t10 for exit...\n-->");
+            int choice;
+            scanf("%d",&choice);
+            switch (choice){
+            case 1:
+                  // int arr [] ={1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+                  printf("Default Tree...\n");
+                  int arr[] ={1, 2, 3, -1, -1, -1, 4, 5, -1, -1, 6, -1, -1};
+                  root= tree(arr);
+                  preOrder(root);
+                  break;
+            case 2:
+                  arrToTree();
+                  break;
+            case 3:
+                  printf("InOrder traversing...\n");
+                  inOrder(root);
+                  break;
+            case 4:
+                  printf("PreOrder traversing...\n");
+                  preOrder(root);
+                  break;
+            case 5:
+                  printf("PostOrder traversing...\n");
+                  postOrder(root);
+                  break;
+            case 6:
+                  printf("Number of nodes in given Tree...\n");
+                  printf("%d", numberOfNodes(root));
+                  break;
+            case 7:
+                  printf("Height of given Tree...\n");
+                  printf("%d", height(root));
+                  break;
+            case 8:
+                  printf("Sum of nodes in given Tree...\n");
+                  printf("%d\n", nodeSum(root));
+                  break;
+            case 9:
+                  printf("Diameter of given Tree...\n");
+                  struct Info * diaInfo = diameterOfTree(root);
+                  printf("%d", diaInfo->diameter);
+                  break;
+            case 10:
+                  printf("Thank you !!!\n");
+                  exit(0);
+            default:
+                  printf("wrong choice !!!\n");
+                  break;
+            }
+      }
 }
